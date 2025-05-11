793760caf2902ad797a8fc3ac34225f835c1398e
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-05-12 03:12:14 +0530

    setting up routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import toast,{Toaster} from 'react-hot-toast'
+const App = () => {
   return (
-  <>
-  <h1>Yo</h1>
-  </>
+    <div data-theme='coffee' className=' h-screen '>
+     
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+         <Route path='/signup' element={<SignUpPage/>}/>
+         <Route path='/login' element={<LoginPage/>}/>
+         <Route path='/notifications' element={<NotificationsPage/>}/>
+         <Route path='/call' element={<CallPage/>}/>
+         <Route path='/chat' element={<ChatPage/>}/>
+         <Route path='/onboarding' element={<OnboardingPage/>}/>
+      </Routes>
+      <Toaster/>
+    </div>
   )
 }
 
