d8f2eb77d6abc7170022ae2da18df680b8131c68
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-05-12 22:34:26 +0530

    signup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv/config'
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import chatRoutes from './routes/chat.route.js'
+import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import cookieParser from "cookie-parser"
 const app=express()
 const PORT=process.env.PORT
-
+app.use(cors(
+   { origin:"http://localhost:5173",
+    credentials:true
+   }
+))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,39 @@ import SignUpPage from './pages/SignUpPage.jsx'
 import OnboardingPage from './pages/OnboardingPage.jsx'
 import NotificationsPage from './pages/NotificationsPage.jsx'
 import toast,{Toaster} from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import { Navigate } from 'react-router'
+import axios from 'axios'
+import { axiosInstance } from './lib/axios.js'
 const App = () => {
+  const {data:authData,isLoading,error}=useQuery({
+    queryKey:["authUser"],
+    queryFn:async()=>{
+      const res=await axiosInstance.get("/auth/me")
+      
+      return res.data
+    },
+    retry:false
+  })
+  const authUser=authData?.user
+  
+  console.log(authUser);
+ 
+  
+
+
+  
   return (
     <div data-theme='coffee' className=' h-screen '>
      
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path='/' element={authUser?<HomePage/>:<Navigate to='/'/>}/>
          <Route path='/signup' element={<SignUpPage/>}/>
          <Route path='/login' element={<LoginPage/>}/>
-         <Route path='/notifications' element={<NotificationsPage/>}/>
-         <Route path='/call' element={<CallPage/>}/>
-         <Route path='/chat' element={<ChatPage/>}/>
-         <Route path='/onboarding' element={<OnboardingPage/>}/>
+         <Route path='/notifications' element={authUser?<NotificationsPage/>:<Navigate to='/'/>}/>
+         <Route path='/call' element={authUser?<CallPage/>:<Navigate to='/'/>}/>
+         <Route path='/chat' element={authUser?<ChatPage/>:<Navigate to='/'/>}/>
+         <Route path='/onboarding' element={authUser?<OnboardingPage/>:<Navigate to='/'/>}/>
       </Routes>
       <Toaster/>
     </div>
