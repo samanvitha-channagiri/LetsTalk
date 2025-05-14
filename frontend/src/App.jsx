6332f6149b0840190f76c8dad7b2966d9472e876
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-05-14 19:37:26 +0530

    onboarding page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,78 @@
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage.jsx'
-import ChatPage from './pages/ChatPage.jsx'
-import CallPage from './pages/CallPage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import SignUpPage from './pages/SignUpPage.jsx'
-import OnboardingPage from './pages/OnboardingPage.jsx'
-import NotificationsPage from './pages/NotificationsPage.jsx'
-import toast,{Toaster} from 'react-hot-toast'
-import { useQuery } from '@tanstack/react-query'
-import { Navigate } from 'react-router'
-import axios from 'axios'
-import { axiosInstance } from './lib/axios.js'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import toast, { Toaster } from "react-hot-toast";
+import { Navigate } from "react-router";
+import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 const App = () => {
-  const {data:authData,isLoading,error}=useQuery({
-    queryKey:["authUser"],
-    queryFn:async()=>{
-      const res=await axiosInstance.get("/auth/me")
-      
-      return res.data
-    },
-    retry:false
-  })
-  const authUser=authData?.user
-  
+  //isLoading and authUser are objects,that's why later we convert to booleans
+  const { isLoading, authUser } = useAuthUser();
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
+  if (isLoading) return <PageLoader />;
+
   console.log(authUser);
- 
-  
 
-
-  
   return (
-    <div data-theme='coffee' className=' h-screen '>
-     
+    <div data-theme="coffee" className=" h-screen ">
       <Routes>
-        <Route path='/' element={authUser?<HomePage/>:<Navigate to='/'/>}/>
-         <Route path='/signup' element={<SignUpPage/>}/>
-         <Route path='/login' element={<LoginPage/>}/>
-         <Route path='/notifications' element={authUser?<NotificationsPage/>:<Navigate to='/'/>}/>
-         <Route path='/call' element={authUser?<CallPage/>:<Navigate to='/'/>}/>
-         <Route path='/chat' element={authUser?<ChatPage/>:<Navigate to='/'/>}/>
-         <Route path='/onboarding' element={authUser?<OnboardingPage/>:<Navigate to='/'/>}/>
+        <Route
+          path="/"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            isAuthenticated ? <NotificationsPage /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/chat"
+          element={isAuthenticated ? <ChatPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/onboarding"
+          element={
+            isAuthenticated ? (
+              !isOnboarded ? (
+                <OnboardingPage />
+              ) : (
+                <Navigate to="/" />
+              )
+            ) : (
+              <Navigate to="login" />
+            )
+          }
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
