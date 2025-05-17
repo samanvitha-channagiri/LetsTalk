c560e86af4553db28c104b380cb2d9e889f0b2e3
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-05-17 06:19:52 +0530

    changes for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv/config'
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import chatRoutes from './routes/chat.route.js'
+import path from 'path'
 import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import cookieParser from "cookie-parser"
 const app=express()
-const PORT=process.env.PORT
+const PORT=process.env.PORT;
+const __dirname=path.resolve()
 app.use(cors(
    { origin:"http://localhost:5173",
     credentials:true
@@ -18,6 +20,12 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/chat',chatRoutes);
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+    app.get("*",(req,res)=>{
+       res.sendFile(path.join(__dirname,"../frontend","dist","index.html")) 
+    })
+}
 
 app.listen(PORT,()=>{
     
